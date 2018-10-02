63e1971e5e093a38de70bc6b144495895a7ec1e7
boddahburnout <dvega9876@gmail.com>
2018-10-02 14:20:51 -0400

    Update Botfuzzy77.py

--- Botfuzzy77.py
+++ Botfuzzy77.py
@@ -415,4 +415,4 @@ async def startpoll(ctx):
     await bot.send_message(channel, embed=Embed)
 
 if __name__ == '__main__':
-    bot.run('MzE1NDczMzkzNDAxMjAwNjUx.DjAmBw.x063UUSqYklv12tj7r4ctOXCDzQ')
+    bot.run('MzE1NDczMzkzNDAxMjAwNjUx.DpVIQg.71ZuhDic6NVlyQMK_pZ7pSPfXSc')
