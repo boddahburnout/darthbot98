eca2d98f21bf90065a38b2c082c7afbdf749717b
boddahburnout <dvega9876@gmail.com>
2019-06-18 19:23:49 -0400

    Update and rename Botfuzzy77.bat to darthbot.bat

--- Botfuzzy77.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-C:\Users\Dakota\AppData\Local\Programs\Python\Python36\python C:\Users\Dakota\Downloads\Botfuzzy77-master\Botfuzzy77-master\Botfuzzy77.py
-exit
---
+++ darthbot.bat
@@ -0,0 +1,3 @@
+@echo off
+C:\Users\Dakota\AppData\Local\Programs\Python\Python36\python darthbot.py
+exit
